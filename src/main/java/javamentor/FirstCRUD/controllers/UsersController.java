90f09f77db61a8d85d422e891cebb02c30c8aab7
Daniel <denchik2599@live.ru>
2020-09-03 16:50:03 +0300

    Working end of part 2 (w/o pages)

--- src/main/java/javamentor/FirstCRUD/controllers/UsersController.java
+++ src/main/java/javamentor/FirstCRUD/controllers/UsersController.java
@@ -2,7 +2,7 @@ package javamentor.FirstCRUD.controllers;
 
 import javamentor.FirstCRUD.model.User;
 import javamentor.FirstCRUD.service.UserService;
-import javamentor.FirstCRUD.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,21 +13,19 @@ import java.util.List;
 @Controller
 public class UsersController {
 
-    private UserService userService = new UserServiceImpl();
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView allUsers() {
         List<User> users = userService.allUsers();
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("allUsers");
-        modelAndView.addObject("userList", users);
+        modelAndView.addObject("usersList", users);
         return modelAndView;
     }
-
-    /*@RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView allUsers() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("allUsers");
-        return modelAndView;
-    }*/
 }

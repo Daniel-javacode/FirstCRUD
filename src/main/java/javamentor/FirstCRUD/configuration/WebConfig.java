a5ca45d0094debe8cdb37ac331d2b9f087980098
Daniel <denchik2599@live.ru>
2020-09-03 12:30:05 +0300

    "Hello world" output

--- src/main/java/javamentor/FirstCRUD/configuration/WebConfig.java
+++ src/main/java/javamentor/FirstCRUD/configuration/WebConfig.java
@@ -1,4 +1,4 @@
-package configuration;
+package javamentor.FirstCRUD.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,14 +9,14 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "java")
+@ComponentScan(basePackages = "javamentor.FirstCRUD")
 public class WebConfig {
 
     @Bean
-    ViewResolver viewResolver(){
+    ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/pages/");
-        viewResolver.setSuffix(".html");
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
 }
--- src/main/java/javamentor/FirstCRUD/controllers/UsersController.java
+++ src/main/java/javamentor/FirstCRUD/controllers/UsersController.java
@@ -1,16 +1,17 @@
-package controllers;
+package javamentor.FirstCRUD.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UsersController {
 
-    @GetMapping(value = "/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView allUsers() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("users");
+        modelAndView.setViewName("allUsers");
         return modelAndView;
     }
 }

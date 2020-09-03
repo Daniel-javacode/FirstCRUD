3e681c186e349cfc7548b958b52d6c6ff10ec1ba
Daniel <denchik2599@live.ru>
2020-09-03 14:16:20 +0300

    перестало работать

--- src/main/java/javamentor/FirstCRUD/controllers/UsersController.java
+++ src/main/java/javamentor/FirstCRUD/controllers/UsersController.java
@@ -1,28 +1,33 @@
 package javamentor.FirstCRUD.controllers;
 
 import javamentor.FirstCRUD.model.User;
+import javamentor.FirstCRUD.service.UserService;
+import javamentor.FirstCRUD.service.UserServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class UsersController {
-    private static User user;
 
-    static {
-        user = new User();
-        user.setId(3);
-        user.setName("Вася");
-        user.setSurname("Пупкин");
-        user.setEmail("pupok@gmail.com");
-    }
+    private UserService userService = new UserServiceImpl();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView allUsers() {
+        List<User> users = userService.allUsers();
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("allUsers");
-        modelAndView.addObject("user", user);
+        modelAndView.addObject("userList", users);
         return modelAndView;
     }
+
+    /*@RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView allUsers() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("allUsers");
+        return modelAndView;
+    }*/
 }
